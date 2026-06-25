7154e3e465a7b1aa9f14154028a056da073596a3
secundus <264293735+MonkeyScourge@users.noreply.github.com>
2026-06-24 18:23:27 -0700

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,28 +14,38 @@
 
   # basic error handling: exit if any command fails
 set -e
+
 image_tag=$1
-#amazon_id=$2
-  # get acc_id from parameter store
-amazon_id=(aws ssm get-parameter --name amazon_acc_id --with-decryption | jq -r '.Parameter.Value')
+
+echo -e "\nFetching AWS ID..."
+amazon_id=$(aws ssm get-parameter --name amazon_acc_id --with-decryption | jq -r '.Parameter.Value')
 
   # get aws-login-token for ecr
+echo -e "\nLogging In..."
 aws ecr get-login-password --region us-west-1 | \
   docker login --username AWS \
-  --password-stdin "$amazon_id".dkr.ecr.us-west-1.amazonaws.com
+  --password-stdin "$amazon_id".dkr.ecr.us-west-1.amazonaws.com 2>/dev/null
 
   # pull image from ecr
+echo -e "\nPulling Image..."
 docker pull "$amazon_id".dkr.ecr.us-west-1.amazonaws.com/my-app:"$image_tag"
 
   # stop running container: git.sha tag specifically
-docker stop $(docker ps -q --filter "label=app.version=${CURRENT_SHA}") 2>/dev/null || true
+echo -e "\nStopping Current Image..."
+#docker stop $(docker ps -q --filter "label=app.version=${CURRENT_SHA}") 2>/dev/null || true
+docker stop $(docker ps -q)
 
   # run new container
-docker run -dp \
-  --label "app=myapp" \
-  --label "app.version=${NEW_SHA}"
-  --name "myapp-${NEW_SHA}" \
-  
+echo -e "\nLaunching New Container..."
+docker run -d -p 8080:80 \
+  "$amazon_id".dkr.ecr.us-west-1.amazonaws.com/my-app:"$image_tag"
+#  --label "app=myapp" \
+#  --label "app.version=${NEW_SHA}"
+#  --name "myapp-${NEW_SHA}" \
+#  "$amazon_id".dkr.ecr.us-west-1.amazonaws.com/my-app:"$image_tag"
 
   # prune unused images
-docker prune -a
+echo -e "\nPruning Old Containers..."
+docker image prune -a -f --filter "until=24h"
+
+echo -e "\nDeployment Complete."
